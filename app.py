fcaed300b3d6d68c4066e5822a179a3be4a9dff4
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2023-08-19 11:24:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from sklearn.metrics import r2_score, mean_absolute_error
 
 st.title('Stock Price Predictions')
 st.sidebar.info('Welcome to the Stock Price Prediction App. Choose your options below')
-st.sidebar.info("Created and designed by [Jonaben](https://www.linkedin.com/in/vikas-sharma005/)")
+st.sidebar.info("Created and designed by [Vikas Sharma](https://www.linkedin.com/in/vikas-sharma005/)")
 
 def main():
     option = st.sidebar.selectbox('Make a choice', ['Visualize','Recent Data', 'Predict'])
